98faa7e75e93cf2805500fd62861ed49b9608b8b
yz8868 <108205199+yz8868@users.noreply.github.com>
2023-04-19 15:39:28 -0400

    Update narrowPeak_to_GPSevents.sh

--- scripts/narrowPeak_to_GPSevents.sh
+++ scripts/narrowPeak_to_GPSevents.sh
@@ -6,8 +6,8 @@ cd $WD/scripts
 
 module load r/gcc/4.1.0
 
-mkdir -p $WD/resources/processed_chip/
+mkdir -p $2
 
 Rscript $WD/scripts/narrowpeak_to_GPSevents.r \
-	$WD/scripts/narrowPeak_to_event.txt \
-	$WD/resources/processed_chip/
+	$1 \
+	$2
